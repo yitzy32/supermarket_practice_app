7ab63ee60d5d7ace7143a4a41719a93a3fbdda30
Yitzy Bratt <yitzybratt32@gmail.com>
2021-03-03 15:53:20 -0500

    adds simple methods in models/food.rb

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,21 @@
 class Food < ApplicationRecord
+  def will_capitalize
+    name.capitalize
+  end
+
+  def friendly_created_at
+    created_at.strftime("%b %d, %Y")
+  end
+
+  def friendly_updated_at
+    updated_at.strftime("%b %d, %Y")
+  end
+
+  def tax
+    price * 0.09
+  end
+
+  def total
+    price + tax
+  end
 end
